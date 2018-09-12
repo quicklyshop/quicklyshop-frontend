bb540b18311c2bf3f6e67c7dac6dd4724204dc68
Fabian Ardila <fabian.ardila@mail.escuelaing.edu.co>
2018-09-12 12:06:11 -0500

    Avance UI

--- src/component/view/Main.jsx
+++ src/component/view/Main.jsx
@@ -3,6 +3,11 @@ import { SidenavComponent } from './SidenavComponent';
 import Sidebar from "react-sidebar";
 import Button from '@material-ui/core/Button';
 import { Profile } from './content/Profile';
+import {list2} from 'react-icons-kit/icomoon/list2';
+import {shoppingCart} from 'react-icons-kit/fa/shoppingCart';
+import {signOut} from 'react-icons-kit/fa/signOut';
+import Icon from 'react-icons-kit';
+import './Main.css';
 
 export class Main extends React.Component {
 
@@ -38,9 +43,16 @@ export class Main extends React.Component {
             onSetOpen={this.onSetSidebarOpen}
             styles={{ sidebar: { background: "white" } }}
             >
-            <Button onClick={() => this.onSetSidebarOpen(true)}>
-              Open sidebar
-            </Button>
+            <div id="menu">
+                <Button onClick={() => this.onSetSidebarOpen(true)}>
+                  <div id="open-menu"><Icon icon={list2}/></div>
+                </Button>
+                <div id="logo">QuicklyShop<Icon icon={shoppingCart}/></div>
+                <span id="spacer"></span>
+                <Button>
+                  <div id="logout"><Icon icon={signOut}/></div>
+                </Button>
+            </div>
             <Profile></Profile>
           </Sidebar>
         );
--- src/component/view/SidenavComponent.jsx
+++ src/component/view/SidenavComponent.jsx
@@ -4,15 +4,16 @@ import SvgIcon from 'react-icons-kit';
  
 import { ic_aspect_ratio } from 'react-icons-kit/md/ic_aspect_ratio';
 import { ic_business } from 'react-icons-kit/md/ic_business';
+import {happy2} from 'react-icons-kit/icomoon/happy2'
  
  
 //specify the base color/background of the parent container if needed
 export const SidenavComponent = () => (
     <div style={{background: '#2c3e50', color: '#FFF', width: 220}}> 
-        <SideNav highlightColor='#E91E63' highlightBgColor='#00bcd4' defaultSelected='sales'>       
-            <Nav id='dashboard'>
-                <NavIcon><SvgIcon size={20} icon={ic_aspect_ratio}/></NavIcon>    
-                <NavText> Dashboard </NavText>
+        <SideNav highlightColor='white' highlightBgColor='#89c34b' defaultSelected='profile'>
+            <Nav id='profile'>
+                <NavIcon><SvgIcon size={20} icon={happy2}/></NavIcon>
+                <NavText> Profile </NavText>
             </Nav>
             <Nav id='sales'>
                 <NavIcon><SvgIcon size={20} icon={ic_business}/></NavIcon>
