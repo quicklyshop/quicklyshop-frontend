7251fb576b824a8bba2f79e3e76d3b5bd4112b2c
Alejandro Anzola <anzola95avila@gmail.com>
2018-09-12 00:02:26 -0500

    version de prueba con sidebar

--- src/App.js
+++ src/App.js
@@ -1,19 +1,100 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Sidebar from "react-sidebar";
 
-class App extends Component {
+const mql = window.matchMedia(`(min-width: 800px)`);
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      sidebarDocked: mql.matches,
+      sidebarOpen: false
+    };
+
+    this.mediaQueryChanged = this.mediaQueryChanged.bind(this);
+    this.onSetSidebarOpen = this.onSetSidebarOpen.bind(this);
+
+    const styles = {
+      root: {
+        position: "absolute",
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        overflow: "hidden"
+      },
+      sidebar: {
+        zIndex: 2,
+        position: "absolute",
+        top: 0,
+        bottom: 0,
+        transition: "transform .3s ease-out",
+        WebkitTransition: "-webkit-transform .3s ease-out",
+        willChange: "transform",
+        overflowY: "auto"
+      },
+      content: {
+        position: "absolute",
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        overflowY: "auto",
+        WebkitOverflowScrolling: "touch",
+        transition: "left .3s ease-out, right .3s ease-out"
+      },
+      overlay: {
+        zIndex: 1,
+        position: "fixed",
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        opacity: 0,
+        visibility: "hidden",
+        transition: "opacity .3s ease-out, visibility .3s ease-out",
+        backgroundColor: "rgba(0,0,0,.3)"
+      },
+      dragHandle: {
+        zIndex: 1,
+        position: "fixed",
+        top: 0,
+        bottom: 0
+      }
+    };
+  }
+
+  componentWillMount() {
+    console.log('llama funcion');
+    mql.addListener(this.mediaQueryChanged);
+  }
+
+  componentWillUnmount() {
+    console.log('llama funcion');
+    mql.removeListener(this.mediaQueryChanged);
+  }
+
+  onSetSidebarOpen(open) {
+    console.log('llama funcion');
+    this.setState({ sidebarOpen: open });
+  }
+
+  mediaQueryChanged() {
+    console.log('llama funcion');
+    this.setState({ sidebarDocked: mql.matches, sidebarOpen: false });
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Sidebar
+        sidebar={<b>Nuestro contenido</b>}
+        open={this.state.sidebarOpen}
+        docked={this.state.sidebarDocked}
+        onSetOpen={this.onSetSidebarOpen}
+        styles={this.styles}
+      >
+        <b>Contenido en la pagina</b>
+      </Sidebar>
     );
   }
 }
